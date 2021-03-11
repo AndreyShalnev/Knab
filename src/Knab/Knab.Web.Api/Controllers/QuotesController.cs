24980b0b7851cbeb5ce8cd1cf41aadc3923b5b61
Andrey Shalnev <andrey.shalnev90@gmail.com>
2021-03-11 17:05:23 +0200

    Create web api with QuotesController

---
+++ src/Knab/Knab.Web.Api/Controllers/QuotesController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Knab.Data;
+using Knab.Data.Provider;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Knab.Web.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class QuotesController : ControllerBase
+    {
+        private readonly IQuoteService _quoteService;
+        
+        public QuotesController(IQuoteService quoteService)
+        {
+            _quoteService = quoteService;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Quote>> GetLatestQuotes(string cryptoCode)
+        {
+            if (string.IsNullOrWhiteSpace(cryptoCode))
+                return BadRequest("Crypto currency code can not be null.");
+
+            return Ok(_quoteService.GetQuotes(cryptoCode));
+        }
+    }
+}
